b1cb9c7bc33011e4eefd134720fba6d7d135911d
Longho Bernard <blongho02@gmail.com>
2024-05-18 13:20:07 +0200

    Get input till user user enters q to quit

--- employee_salary.py
+++ employee_salary.py
@@ -19,24 +19,33 @@ bonus_per_extra_hour = 100
 
 hours_worked = 0
 
-# getting input from the user
-user_input = input("Please enter the hours worked: ")
+promptUser = True
 
-try:
-    hours_worked = float(user_input)
-    weekly_salary = hourly_wage * hours_worked
+while promptUser:
+    # getting input from the user
+    user_input = input("\nPlease enter the hours worked (Enter 'q' to quit!): ")
+    print()
 
-    if hours_worked < min_working_hours or hours_worked > max_working_hours:
-        print(f"Weekly hours must be between {min_working_hours} and {max_working_hours}.")
+    if user_input.lower() == "q":
+        promptUser = False
+        print(f"Program exited gracefully...\n")
     else:
-        if hours_worked <= normal_work_hours:
-            print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
-            print(f"You did not have any bonus this week.")
-        if hours_worked > normal_work_hours:
-            bonus_hours = hours_worked - normal_work_hours
-            bonus_wage = bonus_hours * bonus_per_extra_hour
-            gross_salay_with_bonus = weekly_salary + bonus_wage
-            print(f"Your weekly salary for {hours_worked} hours is {gross_salay_with_bonus} FCFA.")
-            print(f"You had a bonus of {bonus_wage} FCFA for the {bonus_hours} worked.")
-except Exception as ve:
-    print(f"ERROR occured [{ve}]")
+        try:
+            hours_worked = float(user_input)
+            weekly_salary = hourly_wage * hours_worked
+
+            if hours_worked < min_working_hours or hours_worked > max_working_hours:
+                print(f"Weekly hours must be between {min_working_hours} and {max_working_hours}.")
+            else:
+                if hours_worked <= normal_work_hours:
+                    print(f"Your weekly salary for {hours_worked} hours is {weekly_salary} FCFA.")
+                    print(f"You did not have any bonus this week.")
+                if hours_worked > normal_work_hours:
+                    bonus_hours = hours_worked - normal_work_hours
+                    bonus_wage = bonus_hours * bonus_per_extra_hour
+                    gross_salay_with_bonus = weekly_salary + bonus_wage
+                    print(f"Your weekly salary for {hours_worked} hours is {gross_salay_with_bonus} FCFA.")
+                    print(f"You had a bonus of {bonus_wage} FCFA for the {bonus_hours} worked.")
+        except Exception as ve:
+            print(f"ERROR occured [{ve}]")
+    
